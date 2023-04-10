3f88e435654a9f0d122b8bef03749f76fee2f295
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-04-10 17:42:12 +0200

    New entity, cleanup

--- game/entities.mjs
+++ game/entities.mjs
@@ -3,13 +3,23 @@ import { ResourceType } from "./enums.mjs";
 
 export const EntityFactory = {
     buildings: {
+        createBase: function(x, y, idContainer) {
+            const entity = new Entity(idContainer);
+
+            entity.components.set(Components.Bounds, new Components.Bounds(x, y, 3, 3));
+            entity.components.set(Components.Currency, new Components.Currency(0, ResourceType.GOLD));
+            entity.components.set(Components.Hp, new Components.Hp(50));
+
+            return entity;
+        },
+
         createMine: function(x, y, idContainer) {
             const entity = new Entity(idContainer);
-    
+
             entity.components.set(Components.Bounds, new Components.Bounds(x, y, 2, 2));
             entity.components.set(Components.Currency, new Components.Currency(10, ResourceType.GOLD));
             entity.components.set(Components.Production, new Components.Production(ResourceType.GOLD, 5));
-            entity.components.set(Components.Hp, new Components.Hp(50));
+            entity.components.set(Components.Hp, new Components.Hp(20));
     
             return entity;
         }
--- game/utils.mjs
+++ game/utils.mjs
@@ -1,4 +1,2 @@
-import { Components } from "./components.mjs";
-
 export const clamp = (x, minVal, maxVal) => 
     Math.min(Math.max(x, minVal), maxVal);
